eadf846c49e84d355bee22de76b086dc9a3c1c35
David Hernando Davalillo <davidhernando@gmail.com>
2021-10-21 21:02:55 +0200

    Added Key to CLValue types
    Signed-off-by: David Hernando <david.hernando@make.services>

---
+++ NetCasperSDK/Types/URef.cs
@@ -0,0 +1,41 @@
+using System;
+using Org.BouncyCastle.Utilities.Encoders;
+
+namespace NetCasperSDK.Types
+{
+    public class URef
+    {
+        // ex: "uref-e65e72d685359dd21b1a7d7eff2862895c556067499ae6524e67a0e229ceb49e-007"
+        public AccessRights AccessRights { get; }
+        public byte[] RawBytes { get; }
+
+        protected URef(byte[] rawBytes, AccessRights accessRights)
+        {
+            RawBytes = rawBytes;
+            AccessRights = accessRights;
+        }
+
+        public URef FromRawBytes(byte[] rawBytes, AccessRights accessRights)
+        {
+            return new URef(rawBytes, accessRights);
+        }
+
+        public URef FromString(string value)
+        {
+            if (!value.StartsWith("uref-"))
+                throw new ArgumentOutOfRangeException(nameof(value), "An URef object must start with 'uref-'.");
+
+            var parts = value.Substring(5).Split(new char[] {'-'});
+            if (parts.Length != 2)
+                throw new ArgumentOutOfRangeException(nameof(value),
+                    "An Uref object must end with an access rights suffix.");
+            if (parts[0].Length != 64)
+                throw new ArgumentOutOfRangeException(nameof(value), "An Uref object must contain a 32 byte value.");
+            if (parts[1].Length != 3)
+                throw new ArgumentOutOfRangeException(nameof(value),
+                    "An Uref object must contain a 3 digits access rights suffix.");
+
+            return new URef(Hex.Decode(parts[0]), (AccessRights) uint.Parse(parts[1]));
+        }
+    }
+}
