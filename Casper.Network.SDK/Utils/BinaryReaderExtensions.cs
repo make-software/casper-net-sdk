07d4bb02a9073028f562abded24ea475c4b9a1d0
David Hernando <david.hernando@make.services>
2021-11-23 22:13:54 +0100

    Convert methods from CLValue to native types (cont.)
    Signed-off-by: David Hernando <david.hernando@make.services>

--- Casper.Network.SDK/Utils/BinaryReaderExtensions.cs
+++ Casper.Network.SDK/Utils/BinaryReaderExtensions.cs
@@ -31,5 +31,21 @@ namespace Casper.Network.SDK.Utils
             return PublicKey.FromBytes(reader.ReadBytes(keyAlgo == 0x01 ? KeyAlgo.ED25519.GetKeySizeInBytes() 
                 : KeyAlgo.SECP256K1.GetKeySizeInBytes()));
         }
+
+        public static GlobalStateKey ReadCLGlobalStateKey(this BinaryReader reader)
+        {
+            int keyId = reader.PeekChar();
+            
+            // Era Info serializes as a u64 (8 bytes + 1 tag byte)
+            if (keyId == (char)KeyIdentifier.EraInfo) 
+                return GlobalStateKey.FromBytes(reader.ReadBytes(9));
+            
+            //URef serializes as 33 bytes + 1 tag byte
+            if (keyId == (char) KeyIdentifier.URef)
+                return GlobalStateKey.FromBytes(reader.ReadBytes(34));
+            
+            // all others serialize as 32 bytes + 1 tag byte
+            return GlobalStateKey.FromBytes(reader.ReadBytes(33));
+        }
     }
 }
