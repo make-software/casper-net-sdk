9df286b4366ddec5aa2228f31b6ba18f3d09693c
David Hernando <david.hernando@make.services>
2021-11-27 20:46:48 +0100

    CLTyoeInfo unit tests.
    Signed-off-by: David Hernando <david.hernando@make.services>

--- Casper.Network.SDK/Types/CLTypeInfo.cs
+++ Casper.Network.SDK/Types/CLTypeInfo.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Casper.Network.SDK.Types
 {
     public class CLTypeInfo
@@ -8,6 +10,29 @@ namespace Casper.Network.SDK.Types
         {
             Type = type;
         }
+        
+        public static implicit operator CLTypeInfo(CLType t)
+        {
+            return t switch
+            {
+                CLType.Bool => new CLTypeInfo(CLType.Bool),
+                CLType.I32 => new CLTypeInfo(CLType.I32),
+                CLType.I64 => new CLTypeInfo(CLType.I64),
+                CLType.U8 => new CLTypeInfo(CLType.U8),
+                CLType.U32 => new CLTypeInfo(CLType.U32),
+                CLType.U64 => new CLTypeInfo(CLType.U64),
+                CLType.U128 => new CLTypeInfo(CLType.U128),
+                CLType.U256 => new CLTypeInfo(CLType.U256),
+                CLType.U512 => new CLTypeInfo(CLType.U512),
+                CLType.Unit => new CLTypeInfo(CLType.Unit),
+                CLType.String => new CLTypeInfo(CLType.String),
+                CLType.URef => new CLTypeInfo(CLType.URef),
+                CLType.Any => new CLTypeInfo(CLType.Any),
+                CLType.PublicKey => new CLTypeInfo(CLType.PublicKey),
+                _ => throw new Exception($"Implicit cast not supported for CLType '{t}'.")
+            };
+        }
+
 
         public override bool Equals(object obj)
         {
