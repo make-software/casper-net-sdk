de54fcd93575742dc43b4d75b6ea081f4df1eb99
David Hernando <david.hernando@make.services>
2021-12-09 20:59:01 +0100

    README updated. Fixed failing unit test.
    Signed-off-by: David Hernando <david.hernando@make.services>

--- Casper.Network.SDK.Test/DeployJsonSerializerTest.cs
+++ Casper.Network.SDK.Test/DeployJsonSerializerTest.cs
@@ -10,31 +10,21 @@ namespace NetCasperTest
     public class DeployJsonSerializer
     {
         private string deploy1 = TestContext.CurrentContext.TestDirectory + "/TestData/transfer-deploy.json";
+        private string signer1 = "017b8058863aAd49c7b89C77019CEF3a4D863BDf1C0c61499776F94B18465810f7";
+        private string signature1 =
+            "012A0c5896aB9d6CF029268cf454BA9D42BCD40Bd909C984890F326739eEF13201D1d2A9a95938B2A966F2e650d1bdd80931f0374e3A92403E025b806AA1065109";
         
         [Test]
         public void LoadDeployFromFileTest()
         {
             var deploy = Deploy.Load(deploy1);
 
-            var options = new JsonSerializerOptions()
-            {
-                WriteIndented = true,
-                Converters =
-                {
-                    new PublicKey.PublicKeyConverter(),
-                    new ExecutableDeployItemConverter()
-                }
-            };
-            var json = JsonSerializer.Serialize(deploy);
-            File.WriteAllText("/Users/davidhernando/Documents/Casper/serialized_deploy.json", json);
-            
             Assert.IsNotNull(deploy);
             Assert.IsTrue(deploy.ValidateHashes(out string msg));
+            Assert.AreEqual(string.Empty, msg);
             Assert.AreEqual(1, deploy.Approvals.Count);
-            Assert.AreEqual("017b8058863aAd49c7b89C77019CEF3a4D863BDf1C0c61499776F94B18465810f7", 
-                deploy.Approvals[0].Signer.ToAccountHex());
-            Assert.AreEqual("012A0c5896aB9d6CF029268cf454BA9D42BCD40Bd909C984890F326739eEF13201D1d2A9a95938B2A966F2e650d1bdd80931f0374e3A92403E025b806AA1065109", 
-                deploy.Approvals[0].Signature.ToHexString());
+            Assert.AreEqual(signer1, deploy.Approvals[0].Signer.ToAccountHex());
+            Assert.AreEqual(signature1, deploy.Approvals[0].Signature.ToHexString());
         }
 
         [Test]
@@ -45,11 +35,10 @@ namespace NetCasperTest
             
             Assert.IsNotNull(deploy);
             Assert.IsTrue(deploy.ValidateHashes(out string msg));
+            Assert.AreEqual(string.Empty, msg);
             Assert.AreEqual(1, deploy.Approvals.Count);
-            Assert.AreEqual("017b8058863aAd49c7b89C77019CEF3a4D863BDf1C0c61499776F94B18465810f7", 
-                deploy.Approvals[0].Signer.ToAccountHex());
-            Assert.AreEqual("012A0c5896aB9d6CF029268cf454BA9D42BCD40Bd909C984890F326739eEF13201D1d2A9a95938B2A966F2e650d1bdd80931f0374e3A92403E025b806AA1065109", 
-                deploy.Approvals[0].Signature.ToHexString());
+            Assert.AreEqual(signer1, deploy.Approvals[0].Signer.ToAccountHex());
+            Assert.AreEqual(signature1, deploy.Approvals[0].Signature.ToHexString());
         }
 
         [Test]
@@ -61,7 +50,8 @@ namespace NetCasperTest
             deploy.Save(tmpFile);
 
             var deployCopy = Deploy.Load(tmpFile);
-            Assert.IsTrue(deployCopy.ValidateHashes(out string message));
+            Assert.IsTrue(deployCopy.ValidateHashes(out string msg));
+            Assert.AreEqual(string.Empty, msg);
         }
 
         [Test]
@@ -72,7 +62,8 @@ namespace NetCasperTest
             var json = deploy.SerializeToJson();
 
             var deployCopy = Deploy.Parse(json);
-            Assert.IsTrue(deployCopy.ValidateHashes(out string message));
+            Assert.IsTrue(deployCopy.ValidateHashes(out string msg));
+            Assert.AreEqual(string.Empty, msg);
         }
     }
 }
