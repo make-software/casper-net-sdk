c9e04f9683970a2d3b68654013d0be4a3a3b200a
David Hernando <david.hernando@make.services>
2023-05-13 12:07:06 +0200

    BLOX-2204 Add rpc endpoint chain_get_era_summary

---
+++ Casper.Network.SDK/JsonRpc/ResultTypes/GetEraSummaryResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using Casper.Network.SDK.Types;
+
+namespace Casper.Network.SDK.JsonRpc.ResultTypes
+{
+    /// <summary>
+    /// Result for "chain_get_era_summary" RPC response.
+    /// </summary>
+    public class GetEraSummaryResult : RpcResult
+    {
+        /// <summary>
+        /// The Era summary.
+        /// </summary>
+        [JsonPropertyName("era_summary")]
+        public EraSummary EraSummary { get; init; } 
+    }
+}
