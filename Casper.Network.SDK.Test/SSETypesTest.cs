56e0e0a0f8a0d94745f88266f1c527c30809f7d6
David Hernando <david.hernando@make.services>
2024-06-11 11:33:28 +0200

    CSDK-179 added backwards compatibility to FinalitySignature parsing.
    Signed-off-by: David Hernando <david.hernando@make.services>

--- Casper.Network.SDK.Test/SSETypesTest.cs
+++ Casper.Network.SDK.Test/SSETypesTest.cs
@@ -8,6 +8,40 @@ namespace NetCasperTest
     public class SSETypesTest
     {
         [Test]
+        public void FinalitySignatureV1Test()
+        {
+            string testFile = TestContext.CurrentContext.TestDirectory + "/TestData/finality_signature_v156.json";
+            var json = File.ReadAllText(testFile);
+            
+            var doc = System.Text.Json.JsonDocument.Parse(json);
+
+            json = doc.RootElement.GetProperty("FinalitySignature").GetRawText();
+            // Assert.AreEqual(3, doc.RootElement.EnumerateObject().Count());
+            // Assert.AreEqual("U512", );
+            var value = JsonSerializer.Deserialize<FinalitySignature>(json);
+            Assert.IsNotNull(value);
+
+            Assert.AreEqual(1, value.Version);
+            Assert.AreEqual("d800de72aa40c6df064c714d3a6d8b6fab73f68742d8468b84efd6616bbb10bb", value.BlockHash);
+            Assert.AreEqual(0, value.BlockHeight);
+            Assert.AreEqual(13859, value.EraId);
+            Assert.IsNull(value.ChainNameHash);
+            Assert.AreEqual("01221b61b83b889898363501c7defd7baa6729989d8fce2dfffea4632017fd46cc34b88bc85fa570a6e9ada829c67b9fdaa78e8dee2f07d346bcad0010e1d3df0f", 
+                value.Signature.ToHexString().ToLowerInvariant());
+            Assert.AreEqual("01b71b2d746681b4e0f44ef137d72ee0d42122b08ef569dc65bb0395cb624f99e5", 
+                value.PublicKey.ToString().ToLowerInvariant());
+
+            var v1 = (FinalitySignatureV1)value;
+            
+            Assert.AreEqual("d800de72aa40c6df064c714d3a6d8b6fab73f68742d8468b84efd6616bbb10bb", v1.BlockHash);
+            Assert.AreEqual(13859, v1.EraId);
+            Assert.AreEqual("01221b61b83b889898363501c7defd7baa6729989d8fce2dfffea4632017fd46cc34b88bc85fa570a6e9ada829c67b9fdaa78e8dee2f07d346bcad0010e1d3df0f", 
+                v1.Signature.ToHexString().ToLowerInvariant());
+            Assert.AreEqual("01b71b2d746681b4e0f44ef137d72ee0d42122b08ef569dc65bb0395cb624f99e5", 
+                v1.PublicKey.ToString().ToLowerInvariant());
+        }
+        
+        [Test]
         public void FinalitySignatureV2Test()
         {
             string testFile = TestContext.CurrentContext.TestDirectory + "/TestData/finality_signature_v2.json";
@@ -18,20 +52,18 @@ namespace NetCasperTest
             json = doc.RootElement.GetProperty("FinalitySignature").GetRawText();
             // Assert.AreEqual(3, doc.RootElement.EnumerateObject().Count());
             // Assert.AreEqual("U512", );
-            var value = JsonSerializer.Deserialize<IFinalitySignature>(json);
+            var value = JsonSerializer.Deserialize<FinalitySignature>(json);
             Assert.IsNotNull(value);
 
-            var v2 = value.FinalitySignatureV2;
-            Assert.IsNotNull(v2);
-            Assert.AreEqual("13a5603c1dad7d4e4d2ce81313c35043172e1535363c3ec428f559dff5704ea5", v2.BlockHash);
-            Assert.AreEqual(54, v2.BlockHeight);
-            Assert.AreEqual(5, v2.EraId);
-            Assert.AreEqual("8a09603fc862b15412b60a050d71f69c57601b6da7382dd56b9a3f300822bb75", v2.ChainNameHash);
+            Assert.AreEqual(2, value.Version);
+            Assert.AreEqual("13a5603c1dad7d4e4d2ce81313c35043172e1535363c3ec428f559dff5704ea5", value.BlockHash);
+            Assert.AreEqual(54, value.BlockHeight);
+            Assert.AreEqual(5, value.EraId);
+            Assert.AreEqual("8a09603fc862b15412b60a050d71f69c57601b6da7382dd56b9a3f300822bb75", value.ChainNameHash);
             Assert.AreEqual("01a12e3601b4d5c82177231910adaabf50d8a52416e756efee1694ee7534ae16fdb59a7370d564715615736074850b9255ee246c8ffa2502c167c6ef8a86b06504", 
-                v2.Signature.ToHexString().ToLowerInvariant());
+                value.Signature.ToHexString().ToLowerInvariant());
             Assert.AreEqual("01fed662dc7f1f7af43ad785ba07a8cc05b7a96f9ee69613cfde43bc56bec1140b", 
-                v2.PublicKey.ToString().ToLowerInvariant());
-            
+                value.PublicKey.ToString().ToLowerInvariant());
         }
     }
 }
--- Casper.Network.SDK/SSE/FinalitySignature.cs
+++ Casper.Network.SDK/SSE/FinalitySignature.cs
@@ -8,7 +8,7 @@ namespace Casper.Network.SDK.SSE
     /// <summary>
     /// A validator's signature of a block, confirming it is finalized. Produced in Casper v1.x
     /// </summary>
-    public class FinalitySignatureV1 : FinalitySignature
+    public class FinalitySignatureV1
     {
         /// <summary>
         /// The block hash
@@ -37,10 +37,8 @@ namespace Casper.Network.SDK.SSE
         public Signature Signature { get; init; }
     }
 
-    /// <summary>
-    /// A validator's signature of a block, confirming it is finalized. Produced in Casper v2.x
-    /// </summary>
-    public class FinalitySignatureV2 : FinalitySignatureV1
+
+    internal class FinalitySignatureV2 : FinalitySignatureV1
     {
         /// <summary>
         /// The block height
@@ -55,81 +53,152 @@ namespace Casper.Network.SDK.SSE
         public string ChainNameHash { get; init; }
     }
     
-    [JsonConverter(typeof(FinalitySignature.FinalitySignatureConverter))]
-    public interface IFinalitySignature
+    internal class FinalitySignatureCompat
     {
-        public int Version { get; }
+        [JsonPropertyName("V1")]
+        public FinalitySignatureV1 Version1 { get; init; }
         
-        public FinalitySignatureV1 FinalitySignatureV1 { get; }
+        [JsonPropertyName("V2")]
+        public FinalitySignatureV2 Version2 { get; init; }
         
-        public FinalitySignatureV2 FinalitySignatureV2 { get; }
+        [JsonPropertyName("block_hash")]
+        public string BlockHash { get; init; }
+
+        [JsonPropertyName("era_id")]
+        public ulong EraId { get; init; }
+
+        [JsonPropertyName("public_key")]
+        [JsonConverter(typeof(PublicKey.PublicKeyConverter))]
+        public PublicKey PublicKey { get; init; }
+
+        [JsonPropertyName("signature")]
+        [JsonConverter(typeof(Signature.SignatureConverter))]
+        public Signature Signature { get; init; }
     }
     
     /// <summary>
     /// A validator's signature of a block, confirming it is finalized.
     /// </summary>
-    public class FinalitySignature : IFinalitySignature
+    [JsonConverter(typeof(FinalitySignatureConverter))]
+    public class FinalitySignature
     {
         protected int _version;
 
         /// <summary>
-        /// Returns the version of the transfer.
+        /// Returns the version of the finality signature.
         /// </summary>
         public int Version
         {
             get { return _version; }
         }
         
-        /// <summary>
-        /// Returns the transfer as a Version1 transfer object.
-        /// </summary>
-        FinalitySignatureV1 IFinalitySignature.FinalitySignatureV1 => this as FinalitySignatureV1;
+        protected FinalitySignatureV1 _finalitySignatureV1;
 
-        /// <summary>
-        /// Returns the transfer as a Version2 transfer object.
-        /// </summary>
-        FinalitySignatureV2 IFinalitySignature.FinalitySignatureV2 => this as FinalitySignatureV2;
-
-        /// <summary>
-        /// Json converter class to serialize/deserialize a Block to/from Json
-        /// </summary>
-        public class FinalitySignatureConverter : JsonConverter<IFinalitySignature>
+        public static explicit operator FinalitySignatureV1(FinalitySignature finalitySignature)
         {
-            public override bool CanConvert(Type typeToConvert)
-            {
-                return typeToConvert == typeof(IFinalitySignature) ||
-                       typeToConvert == typeof(FinalitySignature);
-            }
+            if(finalitySignature._version == 1)
+                return finalitySignature._finalitySignatureV1;
 
-            public override IFinalitySignature Read(
+            throw new InvalidCastException("Version2 FinalitySignature cannot be converted to Version1");
+        }
+        
+        public static explicit operator FinalitySignature(FinalitySignatureV1 finalitySignature)
+        {
+            return new FinalitySignature
+            {
+                _version = 1,
+                _finalitySignatureV1 = finalitySignature,
+                BlockHash = finalitySignature.BlockHash,
+                BlockHeight = 0, 
+                EraId = finalitySignature.EraId,
+                PublicKey= finalitySignature.PublicKey,
+                Signature = finalitySignature.Signature,
+                ChainNameHash = null,
+            };
+        }
+        
+        /// <summary>
+        /// The block hash
+        /// </summary>
+        [JsonPropertyName("block_hash")]
+        public string BlockHash { get; init; } 
+       
+       /// <summary>
+        /// The block height
+        /// </summary>
+        [JsonPropertyName("block_height")]
+        public ulong BlockHeight { get; init; }
+        
+        /// <summary>
+        /// The hash of the chain name of the associated block. 
+        /// </summary>
+        [JsonPropertyName("chain_name_hash")]
+        public string ChainNameHash { get; init; }
+
+        /// <summary>
+        /// The block era id.
+        /// </summary>
+        [JsonPropertyName("era_id")]
+        public ulong EraId { get; init; }
+
+        /// <summary>
+        /// Validator public key
+        /// </summary>
+        [JsonPropertyName("public_key")]
+        [JsonConverter(typeof(PublicKey.PublicKeyConverter))]
+        public PublicKey PublicKey { get; init; }
+
+        /// <summary>
+        /// Validator signature
+        /// </summary>
+        [JsonPropertyName("signature")]
+        [JsonConverter(typeof(Signature.SignatureConverter))]
+        public Signature Signature { get; init; }
+        
+        /// <summary>
+        /// Json converter class to serialize/deserialize a FinalitySignature to/from Json
+        /// </summary>
+        public class FinalitySignatureConverter : JsonConverter<FinalitySignature>
+        {
+            public override FinalitySignature Read(
                 ref Utf8JsonReader reader,
                 Type typeToConvert,
                 JsonSerializerOptions options)
             {
                 try
                 {
-                    reader.Read();
-                    var version = reader.GetString();
-                    reader.Read();
-                    switch (version)
+                    var fsCompat = JsonSerializer.Deserialize<FinalitySignatureCompat>(ref reader, options);
+                    if (fsCompat.BlockHash != null)
                     {
-                        case "V1":
+                        var v1 = new FinalitySignatureV1()
                         {
-                            var finalitySignature1 = JsonSerializer.Deserialize<FinalitySignatureV1>(ref reader, options);
-                            reader.Read();
-                            finalitySignature1._version = 1;
-                            return finalitySignature1;
-                        }
-                        case "V2":
-                            var finalitySignature2 = JsonSerializer.Deserialize<FinalitySignatureV2>(ref reader, options);
-                            reader.Read();
-                            finalitySignature2._version = 2;
-                            return finalitySignature2;
-                        default:
-                            throw new JsonException("Expected V1 or V2");
+                            BlockHash = fsCompat.BlockHash,
+                            EraId = fsCompat.EraId,
+                            PublicKey = fsCompat.PublicKey,
+                            Signature = fsCompat.Signature,
+
+                        };
+                        return (FinalitySignature)v1;
+                    }
+                    if (fsCompat.Version1 != null)
+                    {
+                        return (FinalitySignature)fsCompat.Version1;
+                    }
+                    if (fsCompat.Version2 != null)
+                    {
+                        return new FinalitySignature
+                        {
+                            _version = 2,
+                            BlockHash = fsCompat.Version2.BlockHash,
+                            BlockHeight = fsCompat.Version2.BlockHeight,
+                            EraId = fsCompat.Version2.EraId,
+                            PublicKey = fsCompat.Version2.PublicKey,
+                            Signature = fsCompat.Version2.Signature,
+                            ChainNameHash = fsCompat.Version2.ChainNameHash,
+                        };
                     }
 
-                    ;
+                    throw new JsonException("Cannot deserialize FinalitySignature");
                 }
                 catch (Exception e)
                 {
@@ -139,7 +208,7 @@ namespace Casper.Network.SDK.SSE
 
             public override void Write(
                 Utf8JsonWriter writer,
-                IFinalitySignature finalitySignature,
+                FinalitySignature finalitySignature,
                 JsonSerializerOptions options)
             {
                 switch (finalitySignature.Version)
@@ -147,13 +216,13 @@ namespace Casper.Network.SDK.SSE
                     case 1:
                         writer.WritePropertyName("V1");
                         writer.WriteStartObject();
-                        JsonSerializer.Serialize(finalitySignature as FinalitySignatureV1, options);
+                        JsonSerializer.Serialize((FinalitySignatureV1)finalitySignature, options);
                         writer.WriteEndObject();
                         break;
                     case 2:
                         writer.WritePropertyName("V2");
                         writer.WriteStartObject();
-                        JsonSerializer.Serialize(finalitySignature as FinalitySignatureV2, options);
+                        JsonSerializer.Serialize(finalitySignature, options);
                         writer.WriteEndObject();
                         break;
                     default:
