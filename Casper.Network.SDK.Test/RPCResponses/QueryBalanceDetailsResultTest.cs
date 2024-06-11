c139e0d8018f41818beaa823d5bf87db01c95842
David Hernando <david.hernando@make.services>
2024-06-11 18:55:18 +0200

    added unit tests.
    Signed-off-by: David Hernando <david.hernando@make.services>

---
+++ Casper.Network.SDK.Test/RPCResponses/QueryBalanceDetailsResultTest.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using Casper.Network.SDK.JsonRpc.ResultTypes;
+using Casper.Network.SDK.Types;
+using NUnit.Framework;
+using Org.BouncyCastle.Utilities.Encoders;
+
+namespace NetCasperTest.RPCResponses
+{
+    public class GetNodeStatusResultTest
+    {
+        [Test]
+        public void GetBlockResultTest_v200()
+        {
+            string json = File.ReadAllText(TestContext.CurrentContext.TestDirectory +
+                                           "/TestData/query-balance-details-v200.json");
+
+            var result = RpcResult.Parse<QueryBalanceDetailsResult>(json);
+            Assert.IsNotNull(result);
+            Assert.AreEqual("2.0.0", result.ApiVersion);
+            Assert.AreEqual(7, result.Holds.Count);
+            Assert.IsTrue(result.Holds[1].Amount > 0);            
+            Assert.IsTrue(result.Holds[1].Time > 0);            
+            Assert.IsFalse(string.IsNullOrWhiteSpace(result.Holds[1].Proof));            
+            Assert.IsTrue(result.TotalBalance > 0);
+            Assert.IsTrue(result.AvailableBalance > 0);
+            Assert.IsFalse(string.IsNullOrWhiteSpace(result.TotalBalanceProof));            
+        }
+    }
+}
