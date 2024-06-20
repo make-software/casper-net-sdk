da8bd04df763da07ccbd92cc2b4a3ccb0dd3eefb
David Hernando <david.hernando@make.services>
2024-06-20 10:38:28 +0200

    fix AddressableEntityKey serialization to bytes
    Signed-off-by: David Hernando <david.hernando@make.services>

--- Casper.Network.SDK/Types/GlobalStateKey/AddressableEntityKey.cs
+++ Casper.Network.SDK/Types/GlobalStateKey/AddressableEntityKey.cs
@@ -1,36 +1,37 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Casper.Network.SDK.Utils;
 using Org.BouncyCastle.Utilities.Encoders;
 
 namespace Casper.Network.SDK.Types
 {
-    public enum EntityKind {
+    public enum EntityKindEnum {
         /// <summary>
-        /// System variant.
+        /// Package associated with a native contract implementation.
         /// </summary>
         System = 0,
         /// <summary>
-        /// Account variant.
+        /// Package associated with an Account hash.
         /// </summary>
         Account = 1,
         /// <summary>
-        /// SmartContract variant.
+        /// Package associated with Wasm stored on chain.
         /// </summary>
         Contract = 2,
     }
     
-    public static class EntityKindExtensions
+    public static class EntityKinEnumExtensions
     {
-        public static string ToKeyPrefix(this EntityKind kind)
+        public static string ToKeyPrefix(this EntityKindEnum kind)
         {
             switch (kind)
             {
-                case EntityKind.System:
+                case EntityKindEnum.System:
                     return "entity-system-";
-                case EntityKind.Account:
+                case EntityKindEnum.Account:
                     return "entity-account-";
-                case EntityKind.Contract:
+                case EntityKindEnum.Contract:
                     return "entity-contract-";
                 default:
                     return kind.ToString();
@@ -38,18 +39,18 @@ namespace Casper.Network.SDK.Types
         }
     }
     
-    public class AddressableEntityKey : GlobalStateKey
+    public class AddressableEntityKey : GlobalStateKey, IEntityIdentifier
     {
-        public EntityKind Kind { get; init; }
+        public EntityKindEnum Kind { get; init; }
         
         private static string GetPrefix(string key)
         {
-            if (key.StartsWith(EntityKind.System.ToKeyPrefix()))
-                return EntityKind.System.ToKeyPrefix();
-            if (key.StartsWith(EntityKind.Account.ToKeyPrefix()))
-                return EntityKind.Account.ToKeyPrefix();
-            if (key.StartsWith(EntityKind.Contract.ToKeyPrefix()))
-                return EntityKind.Contract.ToKeyPrefix();
+            if (key.StartsWith(EntityKindEnum.System.ToKeyPrefix()))
+                return EntityKindEnum.System.ToKeyPrefix();
+            if (key.StartsWith(EntityKindEnum.Account.ToKeyPrefix()))
+                return EntityKindEnum.Account.ToKeyPrefix();
+            if (key.StartsWith(EntityKindEnum.Contract.ToKeyPrefix()))
+                return EntityKindEnum.Contract.ToKeyPrefix();
 
             throw new Exception("Unexpected key prefix in NamedKeyKey: " + key);
             
@@ -58,21 +59,21 @@ namespace Casper.Network.SDK.Types
         {
             KeyIdentifier = KeyIdentifier.AddressableEntity;
             var prefix = GetPrefix(key);
-            if (EntityKind.System.ToKeyPrefix().Equals(prefix))
-                Kind = EntityKind.System;
-            else if (EntityKind.Account.ToKeyPrefix().Equals(prefix))
-                Kind = EntityKind.Account;
-            else if (EntityKind.Contract.ToKeyPrefix().Equals(prefix))
-                Kind = EntityKind.Contract;
+            if (EntityKindEnum.System.ToKeyPrefix().Equals(prefix))
+                Kind = EntityKindEnum.System;
+            else if (EntityKindEnum.Account.ToKeyPrefix().Equals(prefix))
+                Kind = EntityKindEnum.Account;
+            else if (EntityKindEnum.Contract.ToKeyPrefix().Equals(prefix))
+                Kind = EntityKindEnum.Contract;
         }
 
         public AddressableEntityKey(byte[] key) : this( 
-            key[0] == (byte)EntityKind.System
-                ? EntityKind.System.ToKeyPrefix() + CEP57Checksum.Encode(key.Slice(1))
-                : (key[0] == (byte)EntityKind.Account
-                    ? EntityKind.Account.ToKeyPrefix() + CEP57Checksum.Encode(key.Slice(1))
-                    : (key[0] == (byte)EntityKind.Account
-                        ? EntityKind.Contract.ToKeyPrefix() + CEP57Checksum.Encode(key.Slice(1))
+            key[0] == (byte)EntityKindEnum.System
+                ? EntityKindEnum.System.ToKeyPrefix() + CEP57Checksum.Encode(key.Slice(1))
+                : (key[0] == (byte)EntityKindEnum.Account
+                    ? EntityKindEnum.Account.ToKeyPrefix() + CEP57Checksum.Encode(key.Slice(1))
+                    : (key[0] == (byte)EntityKindEnum.Account
+                        ? EntityKindEnum.Contract.ToKeyPrefix() + CEP57Checksum.Encode(key.Slice(1))
                         : throw new Exception($"Wrong entity tag '{key[0]}' for AddressableEntityKey."))))
         {
         }
@@ -82,8 +83,35 @@ namespace Casper.Network.SDK.Types
             KeyIdentifier = KeyIdentifier.AddressableEntity;
             var tag = reader.ReadByte();
             var addr = reader.ReadBytes(32);
-            Kind = (EntityKind)tag;
+            Kind = (EntityKindEnum)tag;
             Key = Kind.ToKeyPrefix() + Hex.ToHexString(addr);
+        }
+        
+        protected override byte[] _GetRawBytesFromKey(string key)
+        {
+            return this.GetBytes();
+        }
+        
+        public override byte[] GetBytes()
+        {
+            var key = Key.Substring(Key.LastIndexOf('-')+1);
+            var rawBytes = Hex.Decode(key);
+            var ms = new MemoryStream();
+            ms.WriteByte((byte)this.Kind);
+            ms.Write(rawBytes);
+
+            return ms.ToArray();
+        }
+        
+        /// <summary>
+        /// Returns an EntityIdentifier object as defined in the RPC schema for an account hash key.
+        /// </summary>
+        public Dictionary<string, object> GetEntityIdentifier()
+        {
+            return new Dictionary<string, object>
+            {
+                {"EntityAddr", Key}
+            };
         }
     }
 }
