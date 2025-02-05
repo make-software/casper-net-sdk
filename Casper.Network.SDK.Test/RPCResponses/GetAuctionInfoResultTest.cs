587be7a136243b2b3df72e75783b3410a49072d6
David Hernando <david.hernando@make.services>
2025-02-05 19:19:29 +0100

    missed file in last commit
    Signed-off-by: David Hernando <david.hernando@make.services>

--- Casper.Network.SDK.Test/RPCResponses/GetAuctionInfoResultTest.cs
+++ Casper.Network.SDK.Test/RPCResponses/GetAuctionInfoResultTest.cs
@@ -27,12 +27,15 @@ namespace NetCasperTest.RPCResponses
             Assert.AreEqual("020377bc3ad54b5505971e001044ea822a3f6f307f8dc93fa45a05b7463c0a053bed", result.AuctionState.EraValidators[0].ValidatorWeights[4].PublicKey.ToString().ToLower());
             Assert.AreEqual(BigInteger.Parse("10567495110201092"), result.AuctionState.EraValidators[0].ValidatorWeights[4].Weight);
             
-            Assert.AreEqual(3, result.AuctionState.Bids.Count);
-            Assert.AreEqual("01001b79b9a6e13d2b96e916f7fa7dff40496ba5188479263ca0fb2ccf8b714305", result.AuctionState.Bids[0].Unified.PublicKey.ToString().ToLower());
-            Assert.AreEqual(1, result.AuctionState.Bids[0].Unified.Delegators.Count);
-            Assert.AreEqual("018b34b15e023844531621cb52d42e216a2ea56034f0f40bf7cee566c32eae4f83", result.AuctionState.Bids[0].Unified.Delegators[0].DelegatorPublicKey.ToString().ToLower());
-            Assert.AreEqual(BigInteger.Parse("30268476029"), result.AuctionState.Bids[0].Unified.Delegators[0].StakedAmount);
-            Assert.IsNull(result.AuctionState.Bids[0].Unified.Delegators[0].VestingSchedule);
+            Assert.AreEqual(6, result.AuctionState.Bids.Count);
+            Assert.AreEqual(3, result.AuctionState.Bids.Count(b => b.Validator != null));
+            Assert.AreEqual(3, result.AuctionState.Bids.Count(b => b.Delegator != null));
+            Assert.AreEqual("01001b79b9a6e13d2b96e916f7fa7dff40496ba5188479263ca0fb2ccf8b714305", result.AuctionState.Bids[4].Validator.PublicKey.ToString().ToLower());
+            Assert.AreEqual(BigInteger.Parse("908982507030"), result.AuctionState.Bids[4].Validator.StakedAmount);
+            Assert.AreEqual("01001b79b9a6e13d2b96e916f7fa7dff40496ba5188479263ca0fb2ccf8b714305", result.AuctionState.Bids[5].Delegator.ValidatorPublicKey.ToString().ToLower());
+            Assert.AreEqual("018b34b15e023844531621cb52d42e216a2ea56034f0f40bf7cee566c32eae4f83", result.AuctionState.Bids[5].Delegator.DelegatorKind.PublicKey.ToString().ToLower());
+            Assert.AreEqual(BigInteger.Parse("30268476029"), result.AuctionState.Bids[5].Delegator.StakedAmount);
+            Assert.AreEqual("uref-401f87167d733d8dd7d3efbf135a91ccd42fffb77b02d4a0075b963f14f1fbb4-007", result.AuctionState.Bids[5].Delegator.BondingPurse.ToString());
         }
         
         [Test]
